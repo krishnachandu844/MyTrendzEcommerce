53fc8559fc5f53964e3112a3d1947a61f212dd36
krishnachandu <krishnachandu844@gmail.com>
2024-09-05 21:34:45 +0530

    added backend of razorpay

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,10 +3,15 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import Razorpay from "razorpay";
+import bodyParser from "body-parser";
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //SECRET KEY
 const secret = "SECR@#RT";
@@ -29,6 +34,56 @@ const cartSchema = new mongoose.Schema({
 //model //
 const USER = mongoose.model("USER", userSchema);
 const CART = mongoose.model("CART", cartSchema);
+
+//RazorPay
+
+//order razorpay
+app.post("/orders", async (req, res) => {
+  const razorpay = new Razorpay({
+    key_id: "rzp_test_QJTNeOoTovszWR",
+    key_secret: "zecxgy1A9MnIX6eIRh7xdjVF",
+  });
+  console.log(req.body);
+  const options = {
+    amount: req.body.amount,
+    currency: req.body.currency,
+    receipt: "Receipt",
+    payment_capture: 1,
+  };
+  try {
+    const response = await razorpay.orders.create(options);
+    res.json({
+      order_id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+//get razorpay
+app.get("/payment/:paymentId", async (req, res) => {
+  const { paymentId } = req.params;
+  const razorpay = new Razorpay({
+    key_id: "rzp_test_QJTNeOoTovszWR",
+    key_secret: "zecxgy1A9MnIX6eIRh7xdjVF",
+  });
+  try {
+    const payment = await razorpay.payments.fetch(paymentId);
+    if (!payment) {
+      return res.status(400).json({ message: "Error at razorpay loading.." });
+    }
+    res.json({
+      status: payment.status,
+      method: payment.method,
+      amount: payment.amount,
+      currency: payment.currency,
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Failed To Fetch" });
+  }
+});
 
 //signup route
 app.post("/auth/signup", async (req, res) => {
